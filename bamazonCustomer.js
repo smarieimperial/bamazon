ef8c2d3b5de55abb347c3f60ddfd89ce3100f088
stephanie <stephanie99tech@gmail.com>
2019-04-26 18:01:57 -0700

    cleaned up file

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,17 +1,17 @@
 
-// in command line run: nodemon bamazonCustomer.js (done)
-// running this application will first display all of the items available for sale. (done)
-// Include the ids, names, prices, etc. of products for sale (done)
+// My notes: in command line run: nodemon bamazonCustomer.js 
+// running this application will first display all of the items available for sale. 
+// Include the ids, names, prices, etc. of products for sale 
 
-// The app should then prompt users with two messages: (done)
+// The app should then prompt users with two messages: 
 
-// 1.   The first should ask them the ID of the product they would like to buy. (done)
-// 2.   The second message should ask how many units of the product they would like to buy. (done)
+// 1.   The first should ask them the ID of the product they would like to buy. 
+// 2.   The second message should ask how many units of the product they would like to buy. 
 
 // 3.   Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request. (done)
 
 // If not, the app should log a phrase like "Insufficient quantity!", and then prevent the order from going through. 
-// However, if your store does have enough of the product, you should fulfill the customer's order. (done)
+// However, if your store does have enough of the product, you should fulfill the customer's order. 
 
 // This means updating the SQL database to reflect the remaining quantity.
 // Once the update goes through, show the customer the total cost of their purchase.
